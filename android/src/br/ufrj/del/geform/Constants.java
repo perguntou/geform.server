40b179f02520773217041edcb3978e0ec98acad5
Bruno@Desktop <bruno.ramos@poli.ufrj.br>
2013-06-16 16:16:11 -0300

    remove constantes desnecessarias

--- android/src/br/ufrj/del/geform/Constants.java
+++ android/src/br/ufrj/del/geform/Constants.java
@@ -4,9 +4,7 @@ package br.ufrj.del.geform;
  * 
  */
 public class Constants {
-	public final static int MAX_TYPE = 2; // TEXT = 0, SINGLECHOICE = 1, MULTIPLECHOICE = 2
-	public final static int TYPE_TEXT = 0, TYPE_SINGLE_CHOICE = 1, TYPE_MULTIPLE_CHOICE = 2;
-	public final static String extension = ".gef";
+	public static final String extension = ".gef";
 
 	private Constants() {}
 }
