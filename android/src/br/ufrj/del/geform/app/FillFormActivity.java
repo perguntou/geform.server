4c647931844811652f81818af156dec54aa970e9
Bruno@Desktop <bruno.ramos@poli.ufrj.br>
2013-06-18 21:33:41 -0300

    remove codigo desnecessario

--- android/src/br/ufrj/del/geform/app/FillFormActivity.java
+++ android/src/br/ufrj/del/geform/app/FillFormActivity.java
@@ -9,10 +9,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 import br.ufrj.del.geform.R;
 import br.ufrj.del.geform.bean.Form;
-import br.ufrj.del.geform.bean.Item;
 
 /**
  *
@@ -58,58 +56,7 @@ public class FillFormActivity extends ListActivity {
 		Intent intent = new Intent( FillFormActivity.this, FormPagerActivity.class );
 		intent.putExtra( "form", (Parcelable) m_form );
 		intent.putExtra( "position", position );
-		startActivityForResult( intent, EDIT_ITEM_REQUEST_CODE );
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
-	 */
-	@Override
-	protected void onActivityResult( int requestCode, int resultCode, Intent result ) {
-		super.onActivityResult( requestCode, resultCode, result );
-
-		if( resultCode == RESULT_OK ) {
-			int resultPosition = checkBoundaries( result.getIntExtra( "resultPosition", m_form.size() ) - 1 ); 
-			Item resultItem = (Item) result.getParcelableExtra( "item" );
-
-			switch( requestCode ) {
-			case ADD_ITEM_REQUEST_CODE:
-				m_form.add( resultPosition, resultItem );
-				break;
-			case EDIT_ITEM_REQUEST_CODE:
-				int requestPosition = result.getIntExtra( "requestPosition", m_form.size() ) - 1;
-				if( requestPosition != resultPosition ) {
-					if( resultPosition > requestPosition ) {
-						resultPosition = Math.min( ++resultPosition, m_form.size() );
-					} else {
-						requestPosition = Math.min( ++requestPosition, m_form.size() );
-					}
-					m_form.add( resultPosition, resultItem );
-					m_form.remove( requestPosition );
-				} else {
-					m_form.set( resultPosition, resultItem );
-				}
-				break;
-			default:
-				return;
-			}
-			((ItemAdapter) getListAdapter()).notifyDataSetChanged();
-		}
-	}
-
-	/**
-	 * Validates an index in the form. Limits the position to
-	 * the form's boundaries, if out returns the last valid position.
-	 * @param position the index
-	 * @return a valid position
-	 */
-	private int checkBoundaries( int position ) {
-		if( position < 0 || position > m_form.size() ) {
-			Toast.makeText( getApplicationContext(), R.string.message_index_invalid, Toast.LENGTH_LONG ).show();
-			position = m_form.size();
-		}
-		return position;
+		startActivity( intent );
 	}
 
 }
