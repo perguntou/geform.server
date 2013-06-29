7bd5eeb206cd17bd4d221d2b6998cd8e08d6bfdf
Bruno@Desktop <bruno.ramos@poli.ufrj.br>
2013-06-29 17:38:33 -0300

    substitui botoes por um menu na activity de criacao de um item do formulario

--- android/src/br/ufrj/del/geform/app/EditItemActivity.java
+++ android/src/br/ufrj/del/geform/app/EditItemActivity.java
@@ -2,15 +2,18 @@ package br.ufrj.del.geform.app;
 
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -30,6 +33,7 @@ public class EditItemActivity extends FragmentActivity {
 	 * (non-Javadoc)
 	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
 	 */
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
@@ -38,61 +42,110 @@ public class EditItemActivity extends FragmentActivity {
 
 		setContentView( R.layout.activity_edit_item );
 
+		if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ) {
+			getActionBar().setDisplayHomeAsUpEnabled( true );
+		}
+
 		final EditText questionEditText = ((EditText) findViewById( R.id.item_question ));
 		final EditText positionEditText = ((EditText) findViewById( R.id.item_position ));
-		final EditOptionsFragment editOptionsFragment = (EditOptionsFragment) getSupportFragmentManager().findFragmentById( R.id.fragment_edit_options );
 		final Spinner itemTypeSpinner = ((Spinner) findViewById( R.id.item_type )); 
 
 		positionEditText.setText( String.valueOf( getIntent().getIntExtra( "requestPosition", 1 ) ) );
 		questionEditText.setText( m_item.getQuestion() );
 		final Type type = m_item.getType();
+
+		final boolean isVisible = type != Type.TEXT;
+		changeFragmentVisibility( isVisible );
+
 		itemTypeSpinner.setSelection( type.ordinal() );
 
-		itemTypeSpinner.setOnItemSelectedListener( new OnItemSelectedListener() {
-			@Override
-			public void onItemSelected( AdapterView<?> adapter, View view, int position, long id ) {
-				FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-				transaction = Type.values()[position] != Type.TEXT ? transaction.show( editOptionsFragment ) : transaction.hide( editOptionsFragment );
-				transaction.commit();
-			}
+		itemTypeSpinner.setOnItemSelectedListener(
+				new OnItemSelectedListener() {
+					@Override
+					public void onItemSelected( AdapterView<?> adapter, View view, int position, long id ) {
+						final Type selectedType = Type.values()[position]; 
+						final boolean isVisible = selectedType != Type.TEXT;
 
-			@Override
-			public void onNothingSelected( AdapterView<?> adapter ) {}
-		} );
-
-		((Button) findViewById( R.id.button_item_ok )).setOnClickListener( new View.OnClickListener() {
-			@Override
-			public void onClick( View view ) {
-				m_item.setQuestion( questionEditText.getText().toString().trim() );
-
-				final String question = m_item.getQuestion();
-				if( question.equals("") ) {
-					Toast.makeText( getApplicationContext(),  R.string.message_question_missing, Toast.LENGTH_LONG ).show();
-					return;
-				}
-				List<String> options = editOptionsFragment.getOptions();
-				final int typeOrdinal = itemTypeSpinner.getSelectedItemPosition();
-				final Type type = Type.values()[typeOrdinal];
-				m_item.setType( type );
-				if( editOptionsFragment.isVisible() ) {
-					if( options.size() < Constants.MIN_NUMBER_OPTIONS ) {
-						final String format = getString( R.string.message_number_options_invalid );
-						Toast.makeText( getApplicationContext(),  String.format( format, Constants.MIN_NUMBER_OPTIONS ), Toast.LENGTH_LONG ).show();
-						return;
+						changeFragmentVisibility( isVisible );
 					}
-					m_item.setOptions( options );
-				} else {
-					m_item.getOptions().clear();
+					@Override
+					public void onNothingSelected( AdapterView<?> adapter ) {}
 				}
+		);
 
-				Intent intent = getIntent();
-				intent.putExtra( "resultPosition", Integer.parseInt( positionEditText.getText().toString() ) );
-				intent.putExtra( "item", m_item );
-				setResult( Activity.RESULT_OK,  intent );
-				finish();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu( Menu menu ) {
+		getMenuInflater().inflate( R.menu.menu_edit_item, menu );
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onOptionsItemSelected( MenuItem item ) {
+		switch( item.getItemId() ) {
+		case android.R.id.home:
+			onBackPressed();
+			break;
+		case R.id.menu_item_accept:
+			final EditText questionEditText = ((EditText) findViewById( R.id.item_question ));
+			final EditText positionEditText = ((EditText) findViewById( R.id.item_position ));
+			final EditOptionsFragment editOptionsFragment = (EditOptionsFragment) getSupportFragmentManager().findFragmentById( R.id.fragment_edit_options );
+			final Spinner itemTypeSpinner = ((Spinner) findViewById( R.id.item_type )); 
+
+			m_item.setQuestion( questionEditText.getText().toString().trim() );
+
+			final String question = m_item.getQuestion();
+			if( question.equals("") ) {
+				Toast.makeText( getApplicationContext(),  R.string.message_question_missing, Toast.LENGTH_LONG ).show();
+				return false;
 			}
-		} );
+			List<String> options = editOptionsFragment.getOptions();
+			final int typeOrdinal = itemTypeSpinner.getSelectedItemPosition();
+			final Type type = Type.values()[typeOrdinal];
+			m_item.setType( type );
+			if( editOptionsFragment.isVisible() ) {
+				if( options.size() < Constants.MIN_NUMBER_OPTIONS ) {
+					final String format = getString( R.string.message_number_options_invalid );
+					Toast.makeText( getApplicationContext(),  String.format( format, Constants.MIN_NUMBER_OPTIONS ), Toast.LENGTH_LONG ).show();
+					return false;
+				}
+				m_item.setOptions( options );
+			} else {
+				m_item.getOptions().clear();
+			}
 
+			Intent intent = getIntent();
+			intent.putExtra( "resultPosition", Integer.parseInt( positionEditText.getText().toString() ) );
+			intent.putExtra( "item", m_item );
+			setResult( Activity.RESULT_OK,  intent );
+			finish();
+			break;
+		default:
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Changes the visibility of options's insertion fragment.
+	 * @param isVisible
+	 */
+	private void changeFragmentVisibility( boolean isVisible )
+	{
+		final EditOptionsFragment editOptionsFragment = (EditOptionsFragment) getSupportFragmentManager().findFragmentById( R.id.fragment_edit_options );
+
+		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		transaction =  isVisible ? transaction.show( editOptionsFragment ) : transaction.hide( editOptionsFragment );
+		transaction.commit();
 	}
 
 	/**
