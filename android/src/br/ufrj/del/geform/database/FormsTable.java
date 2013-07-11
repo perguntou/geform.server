a457fb36a2be9e859901f0c00ac57f0314ca6cb5
Bruno@Desktop <bruno.ramos@poli.ufrj.br>
2013-07-10 21:40:07 -0300

    cria classe com constantes relacionadas ? tabela do banco de dados referente aos formularios

---
+++ android/src/br/ufrj/del/geform/database/FormsTable.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package br.ufrj.del.geform.database;
+
+import android.provider.BaseColumns;
+
+/**
+ *
+ */
+public class FormsTable implements BaseColumns {
+
+	public static final String TABLE_FORMS = "forms";
+	public static final String COLUMN_TITLE = "title";
+
+	public static final String CREATE = "create table if not exists " + TABLE_FORMS + " (" +
+			_ID + " integer primary key, " +
+			COLUMN_TITLE + " text not null" +
+			");";
+
+	public static final String DROP = "drop table if exists" + TABLE_FORMS;
+
+}
