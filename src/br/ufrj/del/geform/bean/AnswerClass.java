5a1462024f26d009ef619e791eab8e960f439942
Bruno@Desktop <bruno.ramos@poli.ufrj.br>
2013-09-16 21:24:38 -0300

    classes basicas relacionadas as coletas

---
+++ src/br/ufrj/del/geform/bean/AnswerClass.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package br.ufrj.del.geform.bean;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ */
+@XmlRootElement
+public class AnswerClass {
+
+	List<String> m_answer;
+
+	public AnswerClass() {}
+
+	/**
+	 * @return the answer
+	 */
+	public List<String> getAnswer() {
+		return m_answer;
+	}
+
+	/**
+	 * @param answer the answer to set
+	 */
+	public void setAnswer( List<String> answer ) {
+		this.m_answer = answer;
+	}
+
+	public void setAnswer( String... strings ) {
+		this.m_answer = Arrays.asList( strings );
+	}
+
+}
