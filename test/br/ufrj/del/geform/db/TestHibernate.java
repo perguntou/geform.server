e2d60ec518508904423b752d71a55e77259c19ff
perguntou <perguntou@poli.ufrj.br>
2013-10-08 23:17:38 -0300

    Criei a fun??o para selecionar um formul?rio do bando de dados e devolver um formul?rio da classe FormBeam. (Fixes issue 043)

--- test/br/ufrj/del/geform/db/TestHibernate.java
+++ test/br/ufrj/del/geform/db/TestHibernate.java
@@ -4,12 +4,17 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import br.ufrj.del.geform.bean.FormBean;
 import br.ufrj.del.geform.bean.ItemBean;
 import br.ufrj.del.geform.bean.OptionBean;
 import br.ufrj.del.geform.bean.TypeBean;
 
 import br.ufrj.del.geform.db.DatabaseManager;
+import br.ufrj.del.geform.db.model.Form;
+import br.ufrj.del.geform.db.model.FormItem;
+import br.ufrj.del.geform.db.model.Item;
+import br.ufrj.del.geform.db.model.Options;
 
 //import javax.persistence.EntityManager;
 //import javax.persistence.EntityManagerFactory;
@@ -31,97 +36,62 @@ import br.ufrj.del.geform.db.DatabaseManager;
 
 public class TestHibernate {
 	public static void main(String[] args) {
-//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("geformdb");
-//		EntityManager em = factory.createEntityManager();
-//		
-//		Type t1 = new Type();
-//		t1.setValue("Texto");
-//		Type t2 = new Type();
-//		t2.setValue("Escolha Única");
-//		
+
+
+//		ItemBean item1 = new ItemBean();
+//		item1.setQuestion("Qual seu nome?");
+//		item1.setType(TypeBean.TEXT);
 //
-//		Form f = new Form();
-//		f.setCreator("Eu");
-//		f.setDescription("Desc.");
-//		f.setTitle("Meu Form");
-//		f.setTimestamp(Date.valueOf("2013-07-26"));
-//		
-//		
-//		Item i1 = new Item();
-//		i1.setQuestion("Qual seu nome?");
-//		i1.setTypeId(20L);
-//		
-//		Item i2 = new Item();
-//		i2.setQuestion("Qual a cor da casa?");
-//		i2.setTypeId(21L);
-//		
-//		Collection c = new Collection();
-//		c.setCollector("Recenciador");
-//		
-//		Options o = new Options();
-//		o.setValue("Op");
-//		
-//		Choice choice = new Choice();
-//		choice.setCollectionId(3L);
-//		choice.setItemId(10L);
-//		choice.setOptionId(1L);
-//		
-//		FormCollection fc = new FormCollection();
-//		fc.setFormId(11L);
-//		fc.setCollectionId(3L);
-//		
-//		FormItem fi1 = new FormItem();
-//		fi1.setFormId(f.getId());
-//		fi1.setItemId(i1.getId());
-//		fi1.setIndex(1);
-//		
-//		FormItem fi2 = new FormItem();
-//		fi2.setFormId(f.getId());
-//		fi2.setItemId(i2.getId());
-//		fi2.setIndex(2);
-//		
-//		ItemOption io = new ItemOption();
-//		io.setItemId(10L);
-//		io.setOptionId(1L);
-//		io.setIndex(1);
-//		
-//		Text text = new Text();
-//		text.setCollectionId(3L);
-//		text.setItemId(9L);
-//		text.setValue("Resposta");
-
-		ItemBean item1 = new ItemBean();
-		item1.setQuestion("Qual seu nome?");
-		item1.setType(TypeBean.TEXT);
-
-		OptionBean opt1 = new OptionBean();
-		opt1.setValue("Option 1");
-		OptionBean opt2 = new OptionBean();
-		opt2.setValue("Option 2");
-		OptionBean opt3 = new OptionBean();
-		opt3.setValue("Option 3");
-
-		ItemBean item2 = new ItemBean();
-		item2.setQuestion("Quais suas preferências?");
-		List<OptionBean> options = new ArrayList<>();
-		options.add(opt1);
-		options.add(opt2);
-		options.add(opt3);
-		item2.setOptions(options);
-		item2.setType(TypeBean.MULTIPLE_CHOICE);
-
-		FormBean form = new FormBean();
-		form.setCreator("Diego");
-		form.setDescription("Teste de inserção.");
-		form.setTimestamp(Date.valueOf("2013-09-10"));
-		form.setTitle("Teste");
-		List<ItemBean> items = new ArrayList<>();
-		items.add(item1);
-		items.add(item2);
-		form.setItems(items);
+//		OptionBean opt1 = new OptionBean();
+//		opt1.setValue("Option 1");
+//		OptionBean opt2 = new OptionBean();
+//		opt2.setValue("Option 2");
+//		OptionBean opt3 = new OptionBean();
+//		opt3.setValue("Option 3");
+//
+//		ItemBean item2 = new ItemBean();
+//		item2.setQuestion("Quais suas preferências?");
+//		List<OptionBean> options = new ArrayList<>();
+//		options.add(opt1);
+//		options.add(opt2);
+//		options.add(opt3);
+//		item2.setOptions(options);
+//		item2.setType(TypeBean.MULTIPLE_CHOICE);
+//
+//		FormBean form = new FormBean();
+//		form.setCreator("Diego");
+//		form.setDescription("Teste de inserção.");
+//		form.setTimestamp(Date.valueOf("2013-09-10"));
+//		form.setTitle("Teste");
+//		List<ItemBean> items = new ArrayList<>();
+//		items.add(item1);
+//		items.add(item2);
+//		form.setItems(items);
 
 		DatabaseManager m = new DatabaseManager();
-		m.insertNewForm( form );
+		
+//		m.insertNewForm( form );
+		
+		FormBean f = m.selectFormBean(12L);
+		
+		System.out.printf("ID %s\n", f.getId().toString());
+		System.out.printf("Tit %s\n", f.getTitle());
+		System.out.printf("Cria %s\n", f.getCreator());
+		System.out.printf("Desc %s\n", f.getDescription());
+		System.out.printf("Data %s\n", f.getTimestamp().toString());
+		
+		for(ItemBean item : f.getItems()){
+			System.out.printf("ID %s\n", item.getId());
+			System.out.printf("Perg %s\n", item.getQuestion());
+			System.out.printf("Type %s\n", item.getType().toString());
+			
+			for(OptionBean option : item.getOptions()){
+				System.out.printf("ID %s\n", option.getId());
+				System.out.printf("Valor %s\n", option.getValue());
+				
+			}
+		}
+
 	}
 
 }
