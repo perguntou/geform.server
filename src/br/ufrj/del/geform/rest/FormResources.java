bc361f583a844dbbab9f818f8af1ab6dc5c7f63c
Bruno@Desktop <bruno.ramos@poli.ufrj.br>
2013-10-09 09:04:53 -0300

    metodo de obter um formulario do servidor classe de teste com cliente para pegar um formulario do servidor
    Resolves issue 39

--- src/br/ufrj/del/geform/rest/FormResources.java
+++ src/br/ufrj/del/geform/rest/FormResources.java
@@ -1,8 +1,11 @@
 package br.ufrj.del.geform.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import br.ufrj.del.geform.bean.FormBean;
@@ -20,6 +23,14 @@ public class FormResources {
 
 		final String result = String.format( "id = %s", form.getId() );
 		return result;
+	}
+
+	@GET
+	@Produces( {MediaType.TEXT_XML, MediaType.APPLICATION_XML} )
+	@Path("{id}")
+	public final FormBean get( @PathParam("id") long id ) {
+		final FormBean form = dbManager.selectFormBean( id );
+		return form;
 	}
 
 }
