580e5fef71905373db2300dd7f6dcdf51cfe2223
Bruno <bruno.ramos@poli.ufrj.br>
2013-10-12 14:33:04 -0300

    pagina inicial do aplicativo web onde se pode procurar um formulario da base pelo ID para visualizar
    Update issue 45
    ao acessar a pagina inicial um campo de busca permite pegar um formulario (pelo ID) no servidor e visualizar suas informacoes.

---
+++ WebContent/includes/template.jsp
@@ -0,0 +1,29 @@
+<div id="template">
+  <!-- Form View -->
+  <div class="form">
+    <div class="formHeader">
+      <h2 class="title"></h2>
+      <span>
+        <label>Id: </label>
+        <span class="id"></span>
+      </span>
+      <span>
+        <label>Creator: </label>
+        <span class="creator"></span>
+      </span>
+      <span>
+        <label>Creation Date: </label>
+        <span class="creationDate"></span>
+      </span>
+      <span>
+        <label>Description: </label>
+        <span class="description"></span>
+      </span>
+    </div>
+    <div class="items"></div>
+  </div>
+  <!-- Item View -->
+  <div class="item">
+    <h3 class="question"></h3>
+  </div>
+</div>
---
+++ WebContent/js/form.js
@@ -0,0 +1,105 @@
+/**
+ * 
+ */
+var INPUT_TYPE = {
+	text : 'text',
+	multiple : 'checkbox',
+	single : 'radio',
+};
+
+var init = function() {
+	try {
+		var $input = $('[id=form]');
+		var $button = $('[id=search]');
+		var $dialog = $( "[id=dialog]" ).dialog(
+		{
+			height: 140,
+			width: 345,
+			autoOpen: false,
+			modal: true
+		} );
+		var callback = function()
+		{
+			try {
+				var value = $input.val();
+				var id = parseInt( value );
+				if( isNaN( id ) ) {
+					$dialog.text('Enter a valid numeric id.');
+					$dialog.dialog('open');
+					return;
+				}
+				var url = "/GeForm/rest/forms/"+id;
+//				window.location.replace( url );
+				var done = function( result, status )
+				{
+					try {
+						var $template = $('[id=template] .form').clone();
+						var $content = $('[id=content]');
+						if( result == null ) {
+							$content.html("");
+							$dialog.text('Could not get the form (id = ' + id + ')');
+							$dialog.dialog('open');
+							return;
+						}
+						insertFieldContent( $template.find('.id'), result.id );
+						insertFieldContent( $template.find('.title'), result.title );
+						insertFieldContent( $template.find('.creator'), result.creator );
+						insertFieldContent( $template.find('.creationDate'), result.timestamp );
+						insertFieldContent( $template.find('.description'), result.description );
+						var insertItem = function( itemIndex, item ) {
+							try {
+								var $itemView = $('[id=template] .item').clone();
+								insertFieldContent( $itemView.find('.question'), item.question );
+								var attr = $(item).attr('@type');
+								if( attr != 'text' ) {
+									var $options = $(document.createElement('span'));
+									var each = function( optionIndex, option ) {
+										var $input = $('<input type=' + INPUT_TYPE[attr] +' value=' + option.id + ' name=' + itemIndex + '>' + option.value +'</input>');
+										$options.append( $input );
+									};
+									$.each( item.options.option, each );
+									$itemView.append($options);
+								} else {
+									$itemView.append( $(document.createElement('textArea') ) );
+								}
+								$template.append( $itemView );
+							} catch( exception ) {
+								var msg = "There was an error on form.init.callback.done.insertItem.\n\nError description: "+ exception.message + "\n\nClick OK to continue.\n\n";
+								alert( msg );
+							}
+						};
+						$.each( result.item, insertItem );
+//						$template.find(':input').attr('disabled',true);
+						$content.html( $template );
+					} catch( exception ) {
+						var msg = "There was an error on form.init.callback.done.\n\nError description: "+ exception.message + "\n\nClick OK to continue.\n\n";
+						alert( msg );
+					}
+				};
+				$.getJSON( url, done );
+			} catch ( exception ) {
+				var msg = "There was an error on form.init.callback.\n\nError description: "+ exception.message + "\n\nClick OK to continue.\n\n";
+				alert( msg );
+			}
+		};
+		$button.click( callback );
+	} catch( exception ) {
+		var msg = "There was an error on form.init.\n\nError description: "+ exception.message + "\n\nClick OK to continue.\n\n";
+		alert( msg );
+	}
+};
+
+function insertFieldContent( view, value ) {
+	try {
+		var $view = $(view);
+		if( value == null || value == "" ) {
+			value = "---";
+		}
+		$view.text( value );
+	} catch( exception ) {
+		var msg = "There was an error on form.insertFieldContent.\n\nError description: " + exception.message + "\n\nClick OK to continue.\n\n";
+		alert( msg );
+	}
+};
+
+$( init );
