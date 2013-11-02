ae921bbeec0468a45948a7d19a76ef769386a612
Bruno@Desktop <bruno.ramos@poli.ufrj.br>
2013-11-02 10:54:54 -0200

    metodo para receber coletas no servidor
    Update issue 55
    Status: Fixed
    recurso criado, retornando o numero de coletas recebidas pelo servidor.

--- src/br/ufrj/del/geform/rest/FormResources.java
+++ src/br/ufrj/del/geform/rest/FormResources.java
@@ -1,5 +1,8 @@
 package br.ufrj.del.geform.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,6 +11,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import br.ufrj.del.geform.bean.CollectionBean;
 import br.ufrj.del.geform.bean.FormBean;
 import br.ufrj.del.geform.db.DatabaseManager;
 
@@ -30,6 +34,27 @@ public class FormResources {
 	public final FormBean get( @PathParam("id") long id ) {
 		final FormBean form = dbManager.selectFormBean( id );
 		return form;
+	}
+
+	@POST
+	@Consumes( MediaType.APPLICATION_XML )
+	@Path("{id}")
+	public final String update( List<CollectionBean> collections, @PathParam("id") final long targetId ) {
+		Integer received = 0;
+		final FormBean form = dbManager.selectFormBean( targetId );
+		final List<CollectionBean> collectionList = new ArrayList<CollectionBean>( collections.size() );
+		for( final CollectionBean collection : collections ) {
+			final long currentId = collection.getId();
+			if( currentId != targetId ) {
+				final String message = String.format( "Form reference (id=%s) for this collection is different from target Form (id=%s)", currentId, targetId );
+				throw new IllegalStateException( message );
+			}
+			collectionList.add( collection );
+			++received;
+		}
+		form.setCollections( collectionList );
+		dbManager.insertCollections( form );
+		return received.toString();
 	}
 
 }
