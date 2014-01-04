7ed9ca0f8f6ba50498dde4f935e23e36258a68cc
Bruno <bruno.ramos@poli.ufrj.br>
2014-01-04 18:07:27 -0200

    utilizando o RequireJS corrige utilizacao do dialog em erro quando se clica no botao de report
    Update issue 63
    Status: Fixed
    Os arquivos necessarios para utilizacao ja haviam sido incluidos na issue 53. Fiz as alteracoes no codigo utilizado atualmente para que use o RequireJS e deve-se seguir utilizando caso novos arquivos sejam incluidos.
    Obs.: Devido ao cache de arquivos feito pelo RequireJS, caso uma modificacao num arquivo de script seja feita n?o ? refletida automaticamente no arquivo utilizado no aplicativo, como anteriormente. Para contornar isso, durante o desenvolvimento, pode-se desabilitar o uso de cache no browser (no Chrome, aperte Ctrl+Shift+I e na aba 'General' das configuracoes marque a opcao de desabilitar o cache).

---
+++ WebContent/js/main.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+requirejs.config( {
+	paths: {
+		jquery: '../libs/jquery/jquery-1.10.2.min',
+		jquery_ui: '../libs/jquery/ui/jquery-ui.min',
+		backbone: '../libs/backbone/backbone.min',
+		underscore: '../libs/underscore/underscore.min',
+		text: '../libs/require/text',
+		templates: '../templates',
+		highcharts: '../libs/highcharts',
+	},
+	shim: {
+		'jquery_ui': {
+			deps: ['jquery']
+		},
+		'backbone': {
+			deps: ['underscore', 'jquery'],
+			exports: 'Backbone'
+		},
+		'underscore': {
+			exports: '_'
+		},
+		'highcharts/modules/exporting': {
+			deps: ['highcharts/highcharts']
+		},
+	},
+} );
+
+require( [
+	'app',
+], function( App ) {
+	App.initialize();
+} );
