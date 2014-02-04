279bfd6d89e685b82ae3c84d0b6b8da22b27dc64
Bruno <bruno.ramos@poli.ufrj.br>
2014-02-03 22:55:52 -0200

    - adiciona funcionalidade de realizar coleta para um formulario - organiza codigo
    Update issue 59
    Status: Fixed
    Agora ? poss?vel realizar coletas para um formul?rio. ? preciso se identificar, assim como responder a todos os itens, para que os dados coletados possam ser enviados para o servidor. Apesar da funcionalidade ter sido criada, todo o layout pode e deve ser ajustado.

--- WebContent/js/report.js
+++ WebContent/js/report.js
@@ -7,140 +7,116 @@ define( [
 	'highcharts/highcharts',
 	'highcharts/modules/exporting',
 ], function( $ ) {
-var init = function() {
+
+var request = function( id ) {
 	try {
-		var $input = $('[id=form]');
-		var $button = $('[id=report]');
-		var $dialog = $( "[id=dialog]" ).dialog(
-		{
-			height: 140,
-			width: 345,
-			autoOpen: false,
-			modal: true
-		} );
-		var callback = function() {
-			try {
-				var value = $input.val();
-				var id = parseInt( value );
-				if( isNaN( id ) ) {
-					$dialog.text('Enter a valid numeric id.');
-					$dialog.dialog('open');
-					return;
-				}
-				var url = "/GeForm/rest/forms/"+id+"/report";
-				var done = function( result, status ) {
-					try {
-						var $content = $('[id=content]');
-
-						$content.html('');
-						if( status != 'success' )
-							return;
-
-						var total = result.total;
-						var $total = $(document.createElement('h1'));
-						$total.text( 'Total: ' + total );
-						$content.append( $total );
-
-						var categories = [];
-						var values = [];
-						var collectors = result.collectors;
-						for( var key in collectors ) {
-							categories.push( key );
-							values.push( collectors[key] );
-						}
-						var $collectors = $(document.createElement('div'));
-						$collectors.highcharts( {
-							chart: {
-								type: 'column',
-							},
-							plotOptions: {
-								column: {
-									showInLegend: false,
-									dataLabels: {
-										enabled: true
-									}
-								},
-							},
-							xAxis: {
-								categories: categories,
-							},
-							tooltip: {
-								enabled: false
-							},
-							series: [{
-								data: values,
-							}],
-							title: {
-								text: 'Collectors',
-							},
-							credits: {
-								enabled: false
-							}
-						} );
-						$content.append( $collectors );
-
-						$.each( result.items, function( index, element ) {
-							var $itemDiv = $(document.createElement('div'));
-							if( element === null ) {
-								var $itemQuestion = $(document.createElement('h1'));
-								$itemQuestion.text( index );
-								$itemDiv.append( $itemQuestion );
-								var $itemQuestion = $(document.createElement('span'));
-								$itemQuestion.text( 'This item could not generate data.' );
-								$itemDiv.append( $itemQuestion );
-							} else {
-								var data = [];
-								for( var key in element ) {
-									data.push( { name: key, y: element[key] } );
-								}
-								$itemDiv.highcharts( {
-									chart: {
-										type: 'pie'
-									},
-									plotOptions: {
-										pie: {
-											allowPointSelect: true,
-											showInLegend: true,
-											dataLabels: {
-												distance: -30,
-												color: 'white'
-											},
-											tooltip: {
-												pointFormat: '<b>{point.y}</b> ({point.percentage:.0f}%)<br/>'
-											}
-										}
-									},
-						            series: [{
-										data: data
-									}],
-									title: {
-										text: index
-									},
-									credits: {
-										enabled: false
-									}
-								} );
-							}
-							$content.append( $itemDiv );
-						} );
-					} catch( exception ) {
-						var msg = "There was an error on report.init.callback.done.\n\nError description: " + exception.message + "\n\nClick OK to continue.\n\n";
-						alert( msg );
-					}
-				};
-				$.getJSON( url, done );
-			} catch ( exception ) {
-				var msg = "There was an error on report.init.callback.\n\nError description: " + exception.message + "\n\nClick OK to continue.\n\n";
-				alert( msg );
-			}
-		};
-		$button.click( callback );
-	} catch( exception ) {
-		var msg = "There was an error on report.init.\n\nError description: " + exception.message + "\n\nClick OK to continue.\n\n";
-		alert( msg );
+		var url = "/GeForm/rest/forms/"+id+"/report";
+		$.getJSON( url, done );
+	} catch ( exception ) {
+		showError( "report.init", exception );
 	}
 };
 
-return { init: init };
+function done( result, status ) {
+	try {
+		var $content = $('[id=content]');
+
+		$content.html('');
+		if( status != 'success' )
+			return;
+
+		var total = result.total;
+		var $total = $(document.createElement('h1'));
+		$total.text( 'Total: ' + total );
+		$content.append( $total );
+
+		var categories = [];
+		var values = [];
+		var collectors = result.collectors;
+		for( var key in collectors ) {
+			categories.push( key );
+			values.push( collectors[key] );
+		}
+		var $collectors = $(document.createElement('div'));
+		$collectors.highcharts( {
+			chart: {
+				type: 'column',
+			},
+			plotOptions: {
+				column: {
+					showInLegend: false,
+					dataLabels: {
+						enabled: true
+					}
+				},
+			},
+			xAxis: {
+				categories: categories,
+			},
+			tooltip: {
+				enabled: false
+			},
+			series: [{
+				data: values,
+			}],
+			title: {
+				text: 'Collectors',
+			},
+			credits: {
+				enabled: false
+			}
+		} );
+		$content.append( $collectors );
+
+		$.each( result.items, function( index, element ) {
+			var $itemDiv = $(document.createElement('div'));
+			if( element === null ) {
+				var $itemQuestion = $(document.createElement('h1'));
+				$itemQuestion.text( index );
+				$itemDiv.append( $itemQuestion );
+				var $itemQuestion = $(document.createElement('span'));
+				$itemQuestion.text( 'This item could not generate data.' );
+				$itemDiv.append( $itemQuestion );
+			} else {
+				var data = [];
+				for( var key in element ) {
+					data.push( { name: key, y: element[key] } );
+				}
+				$itemDiv.highcharts( {
+					chart: {
+						type: 'pie'
+					},
+					plotOptions: {
+						pie: {
+							allowPointSelect: true,
+							showInLegend: true,
+							dataLabels: {
+								distance: -30,
+								color: 'white'
+							},
+							tooltip: {
+								pointFormat: '<b>{point.y}</b> ({point.percentage:.0f}%)<br/>'
+							}
+						}
+					},
+		            series: [{
+						data: data
+					}],
+					title: {
+						text: index
+					},
+					credits: {
+						enabled: false
+					}
+				} );
+			}
+			$content.append( $itemDiv );
+		} );
+	} catch( exception ) {
+		showError( "report.done", exception );
+	}
+};
+
+return { request: request };
 
 } );
