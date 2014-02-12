103b6f2cbc347190856ad314c51df0c57afb40fb
perguntou <perguntou@poli.ufrj.br>
2014-02-12 20:00:24 -0200

    Cria??o do arquivo createForm.js e rosolu??o de problemas no createQuestionView.js.
    update issue 46

--- WebContent/js/createQuestionView.js
+++ WebContent/js/createQuestionView.js
@@ -1,8 +1,19 @@
+define([
+	'jquery',
+	'backbone',
+	'underscore',
+	'jquery_ui'
+], function( $, Backbone, _ ) {
+
 var TextCreateView = Backbone.View.extend({
     tagName: 'div',
     className: 'textQuestion',
-    template: _.template('<h3>Text Question</h3><div><p><textarea class="question" rows="2" cols="80" placeholder="Insert the question" /></p><p><textarea id="answer" rows="4" cols="80"></textarea></p></div>'),
-    
+    template: _.template('\
+    					<h3>Text Question</h3>\
+    					<div>\
+    						<p><textarea class="question" rows="2" cols="80" placeholder="Insert the question"></textarea></p>\
+    						<p><textarea id="answer" rows="4" cols="80"></textarea></p>\
+    					</div>'),
     render: function() {
         this.$el.html(this.template());
     }
@@ -11,25 +22,40 @@ var TextCreateView = Backbone.View.extend({
 var SingleCreateView = Backbone.View.extend({
     tagName: 'div',
     className: 'singleChoiceQuestion',
-    template: _.template('<h3>Single Choice Question</h3><div><p><textarea class="question" rows="2" cols="80" placeholder="Insert the question" /><br><button id="addOption">Add Option</button></p><div class="single-options"></div></div>'),
+    template: _.template('\
+						<h3>Single Choice Question</h3>\
+						<div>\
+							<p>\
+								<textarea class="question" rows="2" cols="80" placeholder="Insert the question"></textarea>\
+								<br><button id="addOption">Add Option</button>\
+							</p>\
+							<div class="single-options"></div>\
+						</div>'),
     events: {
         "click #addOption" : "newOption"
     },
     render: function() {
         this.$el.html(this.template());
     },
-    newOption: function() {
+    newOption: function( event ) {
         var singleOptionCreateView = new SingleOptionCreateView();
-        singleOptionCreateView.render();
-        $('.single-options').append(singleOptionCreateView.el);
-    //##### ERRO => NÃO PODE  USAR A CLASSE, NEM ID FIXO, AGRUPA TODAS Single Choice Questions #####
+        singleOptionCreateView.render(this.cid);
+        this.$el.find('.single-options').append(singleOptionCreateView.el);
     }
 });
 
 var MultipleCreateView = Backbone.View.extend({
     tagName: 'div',
     className: 'multipleChoiceQuestion',
-    template: _.template('<h3>Multiple Choice Question</h3><div><p><textarea class="question" rows="2" cols="80" placeholder="Insert the question" /><br><button id="addOption">Add Option</button></p><div class="multiple-options"></div></div>'),
+    template: _.template('\
+    					<h3>Multiple Choice Question</h3>\
+    					<div>\
+    						<p>\
+    							<textarea class="question" rows="2" cols="80" placeholder="Insert the question"></textarea>\
+    							<br><button id="addOption">Add Option</button>\
+    						</p>\
+    						<div class="multiple-options"></div>\
+    					</div>'),
     events: {
         "click #addOption" : "newOption"
     },
@@ -39,27 +65,42 @@ var MultipleCreateView = Backbone.View.extend({
     newOption: function() {
         var multipleOptionCreateView = new MultipleOptionCreateView();
         multipleOptionCreateView.render();
-        $('.multiple-options').append(multipleOptionCreateView.el);
-      //##### ERRO => NÃO PODE  USAR A CLASSE, NEM ID FIXO, AGRUPA TODAS Multiple Choice Questions #####
+        this.$el.find('.multiple-options').append(multipleOptionCreateView.el);
     }
 });
 
 var SingleOptionCreateView = Backbone.View.extend({
 	tagName: 'li',
     className: 'singleChoiceOption',
-    template: _.template('<input type="radio" name="group1"><input type="text" placeholder="Insert the option">'),
-    //##### ERRO => NÃO PODE DEIXAR O NAME FIXO, AGRUPA TODOS RADIO`S #####
-    render: function() {
-        this.$el.html(this.template());
+    template: _.template('\
+    					<input type="radio" name="<%= name %>">\
+    					<input type="text" placeholder="Insert the option">\
+    					'),
+    render: function(cid) {
+        this.$el.html(this.template({name: cid}));
+        console.log(this.el);
     }
 });
 
 var MultipleOptionCreateView = Backbone.View.extend({
 	tagName: 'li',
     className: 'multipleChoiceOption',
-    template: _.template('<input type="checkbox"><input type="text" placeholder="Insert the option">'),
+    template: _.template('\
+    					<input type="checkbox">\
+    					<input type="text" placeholder="Insert the option">\
+    					'),
     
     render: function() {
         this.$el.html(this.template());
     }
+});
+
+return {
+	TextCreateView: TextCreateView,
+	SingleCreateView: SingleCreateView,
+	MultipleCreateView: MultipleCreateView,
+	SingleOptionCreateView: SingleOptionCreateView,
+	MultipleOptionCreateView: MultipleOptionCreateView
+};
+
 });
