37094ede5b60c5fba4ed66c777676ee5072480f3
Bruno <bruno.ramos@poli.ufrj.br>
2014-02-15 17:48:35 -0200

    trigger da busca com a tecla Enter; corrige verificacao de requisicao por um formulario. Em um commit anterior alterou-se para que caso o formulario nao exista um erro (NOT_FOUND) seja retornado; adiciona title para o botao de busca de formulario; organizacao de codigo.

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -9,38 +9,67 @@ define( [
 	'createForm',
 	'util'
 ], function( $, _, Backbone, Form ) {
+	try {
+		var ENTER_KEY_CODE = 13;
 
-	var initialize = function() {
-		try {
-			var searchInput = $('.searchInput');
-			var searchButton = $('.searchButton');
+		var searchInput = $('.searchInput');
+		var searchButton = $('.searchButton');
 
-			searchInput.attr( 'placeholder', 'Insert the form id' );
-			searchButton.click( function() {
+		function clickHandler( event ) {
+			try {
 				var value = searchInput.val();
 				var id = parseInt( value );
+
 				if( isNaN( id ) ) {
 					showDialog( 'Enter a valid numeric id.' );
-				} else {
-					var url = "/GeForm/rest/forms/"+id;
-					$.getJSON( url, function( result, status ) {
-						if( result == null ) {
-							var $content = $('[id=content]');
-							$content.html("");
-							showDialog( "Could not get the form (id = " + id + ")" );
-						} else {
-							Form.show( result );
-						}
-					} );
+					return false;
 				}
-			} );
-		} catch( exception ) {
-			showError( "app.initialize", exception );
+
+				var success = function( data, status, xhr ) {
+					try {
+						Form.show( data );
+					} catch( exception ) {
+						showError( "app.clickHandler.success", exception );
+					}
+				};
+				var error = function( jqxhr, textStatus, error ) {
+					try {
+						var $content = $('[id=content]');
+						$content.html("");
+						showDialog( "Could not get the form (id = " + id + ")" );
+					} catch( exception ) {
+						showError( "app.clickHandler.error", exception );
+					}
+				};
+
+				var url = "/GeForm/rest/forms/"+id;
+				$.getJSON( url )
+				.done( success )
+				.fail( error );
+			} catch( exception ) {
+				showError( "app.clickHandler", exception );
+			}
 		}
-	};
 
-	return {
-		initialize: initialize
-	};
+		var initialize = function() {
+			try {
+				searchInput.attr( 'placeholder', 'Insert the form id' );
+				searchButton.click( clickHandler );
+				searchButton.attr( 'title', 'Find form' );
+				searchInput.keypress( function( event ) {
+					if( event.which == ENTER_KEY_CODE || event.keyCode == ENTER_KEY_CODE ) {
+						searchButton.click();
+					}
+				} );
+			} catch( exception ) {
+				showError( "app.initialize", exception );
+			}
+		};
 
+		return {
+			initialize: initialize
+		};
+	} catch( exception ) {
+		showError( "app", exception );
+	}
 } );
